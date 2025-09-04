37792a0dcd201e2ac21bde09ae1509aadcb2b9e5
cauã <cauafreirw@gmail.com>
2025-09-04 14:39:54 -0300

    Criando projeto e começando a configurar a parte de pedidos

---
+++ ProjetoPizzariaCidade/Pedido.Designer.cs
@@ -0,0 +1,319 @@
+﻿namespace ProjetoPizzariaCidade
+{
+    partial class Pedido
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnNovo = new System.Windows.Forms.Button();
+            this.cmbCarros = new System.Windows.Forms.ComboBox();
+            this.lblValorCarro = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.grpOpicionais = new System.Windows.Forms.GroupBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.btnSalvar = new System.Windows.Forms.Button();
+            this.btnCalcular = new System.Windows.Forms.Button();
+            this.btnSair = new System.Windows.Forms.Button();
+            this.lblValorPagar = new System.Windows.Forms.Label();
+            this.lblValorOpicionais = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.checkBox3 = new System.Windows.Forms.CheckBox();
+            this.checkBox4 = new System.Windows.Forms.CheckBox();
+            this.checkBox7 = new System.Windows.Forms.CheckBox();
+            this.checkBox8 = new System.Windows.Forms.CheckBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.grpOpicionais.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnNovo
+            // 
+            this.btnNovo.Location = new System.Drawing.Point(703, 499);
+            this.btnNovo.Name = "btnNovo";
+            this.btnNovo.Size = new System.Drawing.Size(100, 46);
+            this.btnNovo.TabIndex = 0;
+            this.btnNovo.Text = "Novo";
+            this.btnNovo.UseVisualStyleBackColor = true;
+            // 
+            // cmbCarros
+            // 
+            this.cmbCarros.FormattingEnabled = true;
+            this.cmbCarros.Location = new System.Drawing.Point(50, 70);
+            this.cmbCarros.Name = "cmbCarros";
+            this.cmbCarros.Size = new System.Drawing.Size(203, 21);
+            this.cmbCarros.TabIndex = 1;
+            this.cmbCarros.Text = "TIPO DE CARRO";
+            // 
+            // lblValorCarro
+            // 
+            this.lblValorCarro.AutoSize = true;
+            this.lblValorCarro.Location = new System.Drawing.Point(455, 78);
+            this.lblValorCarro.Name = "lblValorCarro";
+            this.lblValorCarro.Size = new System.Drawing.Size(103, 13);
+            this.lblValorCarro.TabIndex = 2;
+            this.lblValorCarro.Text = "VALOR DO CARRO";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(367, 305);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(616, 174);
+            this.dataGridView1.TabIndex = 3;
+            // 
+            // grpOpicionais
+            // 
+            this.grpOpicionais.Controls.Add(this.checkBox7);
+            this.grpOpicionais.Controls.Add(this.checkBox8);
+            this.grpOpicionais.Controls.Add(this.checkBox4);
+            this.grpOpicionais.Controls.Add(this.checkBox3);
+            this.grpOpicionais.Controls.Add(this.checkBox2);
+            this.grpOpicionais.Controls.Add(this.checkBox1);
+            this.grpOpicionais.Location = new System.Drawing.Point(50, 248);
+            this.grpOpicionais.Name = "grpOpicionais";
+            this.grpOpicionais.Size = new System.Drawing.Size(178, 231);
+            this.grpOpicionais.TabIndex = 4;
+            this.grpOpicionais.TabStop = false;
+            this.grpOpicionais.Text = "ESCOLHA OS ACESSORIOS";
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.checkBox1.Location = new System.Drawing.Point(38, 39);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(95, 21);
+            this.checkBox1.TabIndex = 0;
+            this.checkBox1.Text = "checkBox1";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(448, 104);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(117, 20);
+            this.textBox1.TabIndex = 5;
+            // 
+            // btnSalvar
+            // 
+            this.btnSalvar.Location = new System.Drawing.Point(551, 499);
+            this.btnSalvar.Name = "btnSalvar";
+            this.btnSalvar.Size = new System.Drawing.Size(100, 46);
+            this.btnSalvar.TabIndex = 6;
+            this.btnSalvar.Text = "Salvar";
+            this.btnSalvar.UseVisualStyleBackColor = true;
+            // 
+            // btnCalcular
+            // 
+            this.btnCalcular.Location = new System.Drawing.Point(394, 499);
+            this.btnCalcular.Name = "btnCalcular";
+            this.btnCalcular.Size = new System.Drawing.Size(100, 46);
+            this.btnCalcular.TabIndex = 7;
+            this.btnCalcular.Text = "Calcular";
+            this.btnCalcular.UseVisualStyleBackColor = true;
+            // 
+            // btnSair
+            // 
+            this.btnSair.Location = new System.Drawing.Point(846, 499);
+            this.btnSair.Name = "btnSair";
+            this.btnSair.Size = new System.Drawing.Size(100, 46);
+            this.btnSair.TabIndex = 8;
+            this.btnSair.Text = "button4";
+            this.btnSair.UseVisualStyleBackColor = true;
+            // 
+            // lblValorPagar
+            // 
+            this.lblValorPagar.AutoSize = true;
+            this.lblValorPagar.Location = new System.Drawing.Point(772, 78);
+            this.lblValorPagar.Name = "lblValorPagar";
+            this.lblValorPagar.Size = new System.Drawing.Size(93, 13);
+            this.lblValorPagar.TabIndex = 9;
+            this.lblValorPagar.Text = "VALOR A PAGAR";
+            // 
+            // lblValorOpicionais
+            // 
+            this.lblValorOpicionais.AutoSize = true;
+            this.lblValorOpicionais.Location = new System.Drawing.Point(587, 78);
+            this.lblValorOpicionais.Name = "lblValorOpicionais";
+            this.lblValorOpicionais.Size = new System.Drawing.Size(141, 13);
+            this.lblValorOpicionais.TabIndex = 10;
+            this.lblValorOpicionais.Text = "VALOR DOS ACESSORIOS";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(445, 248);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(35, 13);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "label4";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(588, 104);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(140, 20);
+            this.textBox2.TabIndex = 12;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(757, 104);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(117, 20);
+            this.textBox3.TabIndex = 13;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(495, 245);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(117, 20);
+            this.textBox4.TabIndex = 14;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(736, 245);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(117, 20);
+            this.textBox5.TabIndex = 15;
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.checkBox2.Location = new System.Drawing.Point(38, 65);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(95, 21);
+            this.checkBox2.TabIndex = 1;
+            this.checkBox2.Text = "checkBox2";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            // 
+            // checkBox3
+            // 
+            this.checkBox3.AutoSize = true;
+            this.checkBox3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.checkBox3.Location = new System.Drawing.Point(38, 92);
+            this.checkBox3.Name = "checkBox3";
+            this.checkBox3.Size = new System.Drawing.Size(95, 21);
+            this.checkBox3.TabIndex = 2;
+            this.checkBox3.Text = "checkBox3";
+            this.checkBox3.UseVisualStyleBackColor = true;
+            // 
+            // checkBox4
+            // 
+            this.checkBox4.AutoSize = true;
+            this.checkBox4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.checkBox4.Location = new System.Drawing.Point(38, 119);
+            this.checkBox4.Name = "checkBox4";
+            this.checkBox4.Size = new System.Drawing.Size(95, 21);
+            this.checkBox4.TabIndex = 3;
+            this.checkBox4.Text = "checkBox4";
+            this.checkBox4.UseVisualStyleBackColor = true;
+            // 
+            // checkBox7
+            // 
+            this.checkBox7.AutoSize = true;
+            this.checkBox7.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.checkBox7.Location = new System.Drawing.Point(38, 172);
+            this.checkBox7.Name = "checkBox7";
+            this.checkBox7.Size = new System.Drawing.Size(95, 21);
+            this.checkBox7.TabIndex = 5;
+            this.checkBox7.Text = "checkBox7";
+            this.checkBox7.UseVisualStyleBackColor = true;
+            // 
+            // checkBox8
+            // 
+            this.checkBox8.AutoSize = true;
+            this.checkBox8.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.checkBox8.Location = new System.Drawing.Point(38, 145);
+            this.checkBox8.Name = "checkBox8";
+            this.checkBox8.Size = new System.Drawing.Size(95, 21);
+            this.checkBox8.TabIndex = 4;
+            this.checkBox8.Text = "checkBox8";
+            this.checkBox8.UseVisualStyleBackColor = true;
+            // 
+            // Pedido
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1060, 600);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.lblValorOpicionais);
+            this.Controls.Add(this.lblValorPagar);
+            this.Controls.Add(this.btnSair);
+            this.Controls.Add(this.btnCalcular);
+            this.Controls.Add(this.btnSalvar);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.grpOpicionais);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.lblValorCarro);
+            this.Controls.Add(this.cmbCarros);
+            this.Controls.Add(this.btnNovo);
+            this.Name = "Pedido";
+            this.Text = "Pedido";
+            this.Load += new System.EventHandler(this.Pedido_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.grpOpicionais.ResumeLayout(false);
+            this.grpOpicionais.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnNovo;
+        private System.Windows.Forms.ComboBox cmbCarros;
+        private System.Windows.Forms.Label lblValorCarro;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.GroupBox grpOpicionais;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button btnSalvar;
+        private System.Windows.Forms.Button btnCalcular;
+        private System.Windows.Forms.Button btnSair;
+        private System.Windows.Forms.Label lblValorPagar;
+        private System.Windows.Forms.Label lblValorOpicionais;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.CheckBox checkBox4;
+        private System.Windows.Forms.CheckBox checkBox3;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.CheckBox checkBox7;
+        private System.Windows.Forms.CheckBox checkBox8;
+    }
+}
---
+++ ProjetoPizzariaCidade/Pedido.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace ProjetoPizzariaCidade
+{
+    public partial class Pedido : Form
+    {
+        public Pedido()
+        {
+            InitializeComponent();
+        }
+
+        private void Pedido_Load(object sender, EventArgs e)
+        {
+            cmbCarros.Items.Add("Sedã");
+            cmbCarros.Items.Add("Toyota Corolla GLi (2025) – R$ 158.490");
+            cmbCarros.Items.Add("Chevrolet Cruze — R$ 140.000");
+            cmbCarros.Items.Add("Volkswagen Jetta GLI — R$ 247.290");
+            cmbCarros.Items.Add("Honda Civic Hatch Type R — R$ 429.990");
+            cmbCarros.Items.Add("SUVs");
+            cmbCarros.Items.Add("Fiat Pulse — R$ 98.990");
+            cmbCarros.Items.Add("Citroën Basalt — R$ 101.490");
+            cmbCarros.Items.Add("Volkswagen Tera — R$ 105.890");
+            cmbCarros.Items.Add("Renault Kardian — R$ 112.690");
+            cmbCarros.Items.Add("Esportivos");
+            cmbCarros.Items.Add("Mazda MX-5 (Miata) — R$ 220.000");
+            cmbCarros.Items.Add("BMW M240i — R$ 380.000");
+            cmbCarros.Items.Add("Ford Mustang EcoBoost — R$ 310.000");
+            cmbCarros.Items.Add("Subaru BRZ — R$ 250.000");
+        }
+    }
+}
