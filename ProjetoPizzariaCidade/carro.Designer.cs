dd79ee092baad84053cae0714b457ef13c7a7e1c
cauã <cauafreirw@gmail.com>
2025-09-10 17:28:02 -0300

    Configurando novamente o form carros

--- ProjetoPizzariaCidade/carro.Designer.cs
+++ ProjetoPizzariaCidade/carro.Designer.cs
@@ -55,6 +55,8 @@
             this.label13 = new System.Windows.Forms.Label();
             this.label14 = new System.Windows.Forms.Label();
             this.label15 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
@@ -75,7 +77,7 @@
             this.label1.BackColor = System.Drawing.Color.Transparent;
             this.label1.Font = new System.Drawing.Font("Lucida Sans Typewriter", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.ForeColor = System.Drawing.Color.White;
-            this.label1.Location = new System.Drawing.Point(601, 9);
+            this.label1.Location = new System.Drawing.Point(591, 9);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(83, 33);
             this.label1.TabIndex = 0;
@@ -87,7 +89,7 @@
             this.label2.BackColor = System.Drawing.Color.Transparent;
             this.label2.Font = new System.Drawing.Font("Lucida Sans Typewriter", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label2.ForeColor = System.Drawing.Color.White;
-            this.label2.Location = new System.Drawing.Point(584, 254);
+            this.label2.Location = new System.Drawing.Point(580, 221);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(100, 33);
             this.label2.TabIndex = 1;
@@ -99,7 +101,7 @@
             this.lblEsportivo.BackColor = System.Drawing.Color.Transparent;
             this.lblEsportivo.Font = new System.Drawing.Font("Lucida Sans Typewriter", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lblEsportivo.ForeColor = System.Drawing.Color.White;
-            this.lblEsportivo.Location = new System.Drawing.Point(534, 473);
+            this.lblEsportivo.Location = new System.Drawing.Point(538, 426);
             this.lblEsportivo.Name = "lblEsportivo";
             this.lblEsportivo.Size = new System.Drawing.Size(185, 33);
             this.lblEsportivo.TabIndex = 2;
@@ -108,7 +110,7 @@
             // pictureBox1
             // 
             this.pictureBox1.Image = global::ProjetoPizzariaCidade.Properties.Resources.Fiat_Pulse_Impetus_T200_Hybrid_2025_10;
-            this.pictureBox1.Location = new System.Drawing.Point(114, 300);
+            this.pictureBox1.Location = new System.Drawing.Point(123, 263);
             this.pictureBox1.Name = "pictureBox1";
             this.pictureBox1.Size = new System.Drawing.Size(171, 129);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
@@ -118,7 +120,7 @@
             // pictureBox2
             // 
             this.pictureBox2.Image = global::ProjetoPizzariaCidade.Properties.Resources.mustang;
-            this.pictureBox2.Location = new System.Drawing.Point(677, 520);
+            this.pictureBox2.Location = new System.Drawing.Point(686, 463);
             this.pictureBox2.Name = "pictureBox2";
             this.pictureBox2.Size = new System.Drawing.Size(171, 129);
             this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
@@ -128,7 +130,7 @@
             // pictureBox3
             // 
             this.pictureBox3.Image = global::ProjetoPizzariaCidade.Properties.Resources.subaru;
-            this.pictureBox3.Location = new System.Drawing.Point(930, 520);
+            this.pictureBox3.Location = new System.Drawing.Point(939, 463);
             this.pictureBox3.Name = "pictureBox3";
             this.pictureBox3.Size = new System.Drawing.Size(171, 129);
             this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
@@ -138,7 +140,7 @@
             // pictureBox4
             // 
             this.pictureBox4.Image = global::ProjetoPizzariaCidade.Properties.Resources.Novo_Citroen_Basalt_First_Edition_2025_3;
-            this.pictureBox4.Location = new System.Drawing.Point(388, 300);
+            this.pictureBox4.Location = new System.Drawing.Point(397, 263);
             this.pictureBox4.Name = "pictureBox4";
             this.pictureBox4.Size = new System.Drawing.Size(171, 129);
             this.pictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
@@ -148,7 +150,7 @@
             // pictureBox5
             // 
             this.pictureBox5.Image = global::ProjetoPizzariaCidade.Properties.Resources.volkswagen_tera_8_t8foeab1nasi;
-            this.pictureBox5.Location = new System.Drawing.Point(671, 300);
+            this.pictureBox5.Location = new System.Drawing.Point(680, 263);
             this.pictureBox5.Name = "pictureBox5";
             this.pictureBox5.Size = new System.Drawing.Size(171, 129);
             this.pictureBox5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
@@ -158,7 +160,7 @@
             // pictureBox6
             // 
             this.pictureBox6.Image = global::ProjetoPizzariaCidade.Properties.Resources.renault_kardian;
-            this.pictureBox6.Location = new System.Drawing.Point(930, 300);
+            this.pictureBox6.Location = new System.Drawing.Point(939, 263);
             this.pictureBox6.Name = "pictureBox6";
             this.pictureBox6.Size = new System.Drawing.Size(171, 129);
             this.pictureBox6.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
@@ -168,7 +170,7 @@
             // pictureBox7
             // 
             this.pictureBox7.Image = global::ProjetoPizzariaCidade.Properties.Resources.honda_civic_sport_typeR;
-            this.pictureBox7.Location = new System.Drawing.Point(930, 59);
+            this.pictureBox7.Location = new System.Drawing.Point(939, 35);
             this.pictureBox7.Name = "pictureBox7";
             this.pictureBox7.Size = new System.Drawing.Size(171, 129);
             this.pictureBox7.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
@@ -178,7 +180,7 @@
             // pictureBox8
             // 
             this.pictureBox8.Image = global::ProjetoPizzariaCidade.Properties.Resources.Volkswagen_Jetta_GLi_TSi_2019_a_venda_em_salvador_bahia_motors_a_venda;
-            this.pictureBox8.Location = new System.Drawing.Point(677, 59);
+            this.pictureBox8.Location = new System.Drawing.Point(686, 35);
             this.pictureBox8.Name = "pictureBox8";
             this.pictureBox8.Size = new System.Drawing.Size(171, 129);
             this.pictureBox8.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
@@ -188,7 +190,7 @@
             // pictureBox9
             // 
             this.pictureBox9.Image = global::ProjetoPizzariaCidade.Properties.Resources.chevroletcruze;
-            this.pictureBox9.Location = new System.Drawing.Point(388, 59);
+            this.pictureBox9.Location = new System.Drawing.Point(397, 35);
             this.pictureBox9.Name = "pictureBox9";
             this.pictureBox9.Size = new System.Drawing.Size(171, 129);
             this.pictureBox9.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
@@ -198,7 +200,7 @@
             // pictureBox10
             // 
             this.pictureBox10.Image = global::ProjetoPizzariaCidade.Properties.Resources.Toyota_Corolla_GLi_2025_10;
-            this.pictureBox10.Location = new System.Drawing.Point(114, 59);
+            this.pictureBox10.Location = new System.Drawing.Point(123, 35);
             this.pictureBox10.Name = "pictureBox10";
             this.pictureBox10.Size = new System.Drawing.Size(171, 129);
             this.pictureBox10.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
@@ -208,7 +210,7 @@
             // pictureBox11
             // 
             this.pictureBox11.Image = global::ProjetoPizzariaCidade.Properties.Resources.mx5mazda;
-            this.pictureBox11.Location = new System.Drawing.Point(114, 520);
+            this.pictureBox11.Location = new System.Drawing.Point(123, 463);
             this.pictureBox11.Name = "pictureBox11";
             this.pictureBox11.Size = new System.Drawing.Size(171, 129);
             this.pictureBox11.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
@@ -218,7 +220,7 @@
             // pictureBox12
             // 
             this.pictureBox12.Image = global::ProjetoPizzariaCidade.Properties.Resources.bmw_m240i;
-            this.pictureBox12.Location = new System.Drawing.Point(383, 520);
+            this.pictureBox12.Location = new System.Drawing.Point(392, 463);
             this.pictureBox12.Name = "pictureBox12";
             this.pictureBox12.Size = new System.Drawing.Size(171, 129);
             this.pictureBox12.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
@@ -231,7 +233,7 @@
             this.label3.BackColor = System.Drawing.Color.Transparent;
             this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label3.ForeColor = System.Drawing.Color.White;
-            this.label3.Location = new System.Drawing.Point(115, 661);
+            this.label3.Location = new System.Drawing.Point(124, 604);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(170, 20);
             this.label3.TabIndex = 15;
@@ -243,7 +245,7 @@
             this.label5.BackColor = System.Drawing.Color.Transparent;
             this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label5.ForeColor = System.Drawing.Color.White;
-            this.label5.Location = new System.Drawing.Point(97, 211);
+            this.label5.Location = new System.Drawing.Point(106, 187);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(214, 20);
             this.label5.TabIndex = 17;
@@ -255,7 +257,7 @@
             this.label6.BackColor = System.Drawing.Color.Transparent;
             this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label6.ForeColor = System.Drawing.Color.White;
-            this.label6.Location = new System.Drawing.Point(408, 211);
+            this.label6.Location = new System.Drawing.Point(417, 187);
             this.label6.Name = "label6";
             this.label6.Size = new System.Drawing.Size(137, 20);
             this.label6.TabIndex = 18;
@@ -267,7 +269,7 @@
             this.label7.BackColor = System.Drawing.Color.Transparent;
             this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label7.ForeColor = System.Drawing.Color.White;
-            this.label7.Location = new System.Drawing.Point(667, 211);
+            this.label7.Location = new System.Drawing.Point(676, 187);
             this.label7.Name = "label7";
             this.label7.Size = new System.Drawing.Size(186, 20);
             this.label7.TabIndex = 19;
@@ -279,7 +281,7 @@
             this.label8.BackColor = System.Drawing.Color.Transparent;
             this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label8.ForeColor = System.Drawing.Color.White;
-            this.label8.Location = new System.Drawing.Point(905, 211);
+            this.label8.Location = new System.Drawing.Point(914, 187);
             this.label8.Name = "label8";
             this.label8.Size = new System.Drawing.Size(218, 20);
             this.label8.TabIndex = 20;
@@ -291,7 +293,7 @@
             this.label9.BackColor = System.Drawing.Color.Transparent;
             this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label9.ForeColor = System.Drawing.Color.White;
-            this.label9.Location = new System.Drawing.Point(157, 443);
+            this.label9.Location = new System.Drawing.Point(166, 406);
             this.label9.Name = "label9";
             this.label9.Size = new System.Drawing.Size(89, 20);
             this.label9.TabIndex = 21;
@@ -303,7 +305,7 @@
             this.label10.BackColor = System.Drawing.Color.Transparent;
             this.label10.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label10.ForeColor = System.Drawing.Color.White;
-            this.label10.Location = new System.Drawing.Point(408, 443);
+            this.label10.Location = new System.Drawing.Point(417, 406);
             this.label10.Name = "label10";
             this.label10.Size = new System.Drawing.Size(123, 20);
             this.label10.TabIndex = 22;
@@ -315,7 +317,7 @@
             this.label11.BackColor = System.Drawing.Color.Transparent;
             this.label11.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label11.ForeColor = System.Drawing.Color.White;
-            this.label11.Location = new System.Drawing.Point(683, 443);
+            this.label11.Location = new System.Drawing.Point(692, 406);
             this.label11.Name = "label11";
             this.label11.Size = new System.Drawing.Size(146, 20);
             this.label11.TabIndex = 23;
@@ -327,7 +329,7 @@
             this.label12.BackColor = System.Drawing.Color.Transparent;
             this.label12.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label12.ForeColor = System.Drawing.Color.White;
-            this.label12.Location = new System.Drawing.Point(945, 443);
+            this.label12.Location = new System.Drawing.Point(954, 406);
             this.label12.Name = "label12";
             this.label12.Size = new System.Drawing.Size(138, 20);
             this.label12.TabIndex = 24;
@@ -339,7 +341,7 @@
             this.label13.BackColor = System.Drawing.Color.Transparent;
             this.label13.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label13.ForeColor = System.Drawing.Color.White;
-            this.label13.Location = new System.Drawing.Point(417, 661);
+            this.label13.Location = new System.Drawing.Point(426, 604);
             this.label13.Name = "label13";
             this.label13.Size = new System.Drawing.Size(104, 20);
             this.label13.TabIndex = 25;
@@ -351,7 +353,7 @@
             this.label14.BackColor = System.Drawing.Color.Transparent;
             this.label14.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label14.ForeColor = System.Drawing.Color.White;
-            this.label14.Location = new System.Drawing.Point(667, 661);
+            this.label14.Location = new System.Drawing.Point(676, 604);
             this.label14.Name = "label14";
             this.label14.Size = new System.Drawing.Size(203, 20);
             this.label14.TabIndex = 26;
@@ -363,11 +365,34 @@
             this.label15.BackColor = System.Drawing.Color.Transparent;
             this.label15.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label15.ForeColor = System.Drawing.Color.White;
-            this.label15.Location = new System.Drawing.Point(958, 661);
+            this.label15.Location = new System.Drawing.Point(967, 604);
             this.label15.Name = "label15";
             this.label15.Size = new System.Drawing.Size(108, 20);
             this.label15.TabIndex = 27;
             this.label15.Text = "Subaru BRZ";
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(1039, 653);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(133, 46);
+            this.button2.TabIndex = 29;
+            this.button2.Text = "Sair";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            this.button2.MouseHover += new System.EventHandler(this.button2_MouseHover);
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(886, 653);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(133, 46);
+            this.button1.TabIndex = 30;
+            this.button1.Text = "Voltar";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // carro
             // 
@@ -376,6 +401,8 @@
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(192)))));
             this.BackgroundImage = global::ProjetoPizzariaCidade.Properties.Resources.ChatGPT_Image_10_de_set__de_2025__16_42_16__1_;
             this.ClientSize = new System.Drawing.Size(1184, 711);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.button2);
             this.Controls.Add(this.label15);
             this.Controls.Add(this.label14);
             this.Controls.Add(this.label13);
@@ -451,5 +478,7 @@
         private System.Windows.Forms.Label label13;
         private System.Windows.Forms.Label label14;
         private System.Windows.Forms.Label label15;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
     }
 }
--- ProjetoPizzariaCidade/carro.cs
+++ ProjetoPizzariaCidade/carro.cs
@@ -16,5 +16,42 @@ namespace ProjetoPizzariaCidade
         {
             InitializeComponent();
         }
+
+        private void button2_MouseHover(object sender, EventArgs e)
+        {
+             
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            DialogResult sair = MessageBox.Show("deseja sair?", "sair ", MessageBoxButtons.YesNo, MessageBoxIcon.Information);
+            if (sair == DialogResult.No)
+            {
+                carro car = new carro();
+                car.Show();
+                this.Hide();
+            }
+            else
+            {
+                Application.Exit();
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            DialogResult sair = MessageBox.Show("deseja Voltar?", "Voltar ", MessageBoxButtons.YesNo, MessageBoxIcon.Information);
+            if (sair == DialogResult.No)
+            {
+                carro car = new carro();
+                car.Show();
+                this.Hide();
+            }
+            else
+            {
+                Menu menu = new Menu();
+                menu.Show();
+                this.Hide();
+            }
+        }
     }
 }
